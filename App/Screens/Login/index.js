bbaa9dc42aa2567982e6542c37d15ec1da3262c4
Johnatan <johnatan@jbotero.com>
2019-10-29 18:44:12 -0500

    Clean Code

--- App/Screens/Login/index.js
+++ App/Screens/Login/index.js
@@ -2,22 +2,18 @@ import {connect} from 'react-redux';
 import Content from './Content';
 
 import {setLoading} from '../../Core/UI/Actions';
-import {getLocations, getPlaces} from '../../Core/Places/Actions';
 
-const mapStateToProps = ({ui, questions}) => {
+const mapStateToProps = ({ui}) => {
   const {loading} = ui;
 
   return {
     loading,
-    questions,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     setLoading: state => dispatch(setLoading(state)),
-    getLocations: (lat, long) => dispatch(getLocations(lat, long)),
-    getPlaces: query => dispatch(getPlaces(query)),
   };
 };
 
